b49aa969d72da4f7d75d1762a60ef36469a20cd5
amanrawat08 <ar7541147@gmail.com>
2025-09-16 19:45:50 +0530

    Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Home from "./Pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Navbar />
-      <Hero />
+      <Home />
     </>
   );
 }
